061b8713656515c409fcc4b4db3ce31883f1ddcd
choikwangil <choikwangil0@naver.com>
2020-10-04 14:39:19 +0900

    [#2] 기본 UI, CSS setting, 위도,경도 받기

---
+++ home/static/js/position.js
@@ -0,0 +1,26 @@
+let position = document.getElementsByClassName("button__position")[0];
+let longitude = document.getElementsByClassName("location__long")[0];
+let latitude = document.getElementsByClassName("location__lat")[0];
+
+position.addEventListener("click", getLocation);
+
+function getLocation() {
+  if (navigator.geolocation) { // GPS를 지원하면
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      let longitudeValue = position.coords.latitude;
+      let latitudeValue = position.coords.longitude;
+      longitude.innerHTML = longitudeValue;
+      latitude.innerHTML = latitudeValue;
+    }, function(error) {
+      console.error(error);
+    }, {
+      enableHighAccuracy: false,
+      maximumAge: 0,
+      timeout: Infinity
+    });
+  } else {
+    alert('GPS를 지원하지 않습니다');
+  }
+}
