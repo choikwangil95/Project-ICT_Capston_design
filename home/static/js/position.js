2f8a14da0f4009bbb6c4306b9a9588b56a967622
choikwangil <choikwangil0@naver.com>
2020-10-10 17:56:13 +0900

    [#2] 스프린트 1 끝~

--- home/static/js/position.js
+++ home/static/js/position.js
@@ -1,16 +1,29 @@
 let position = document.getElementsByClassName("button__position")[0];
-let longitude = document.getElementsByClassName("location__long")[0];
-let latitude = document.getElementsByClassName("location__lat")[0];
-
-position.addEventListener("click", getLocation);
+let address = document.getElementsByClassName("location__address")[0];
+position.addEventListener("click", getLocation, changeMap);
 
 function getLocation() {
   if (navigator.geolocation) { // GPS를 지원하면
     navigator.geolocation.getCurrentPosition(function(position) {
-      let longitudeValue = position.coords.latitude;
-      let latitudeValue = position.coords.longitude;
-      longitude.innerHTML = longitudeValue;
-      latitude.innerHTML = latitudeValue;
+      let lngitudeValue = position.coords.longitude;
+      let latitudeValue = position.coords.latitude;
+
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitudeValue},${lngitudeValue}&key=AIzaSyDbM2ILxG_n0ScqaBRFcf40fCalno5QX90`)
+        .then((res) => {
+          let result = res.data.results[0].formatted_address.slice(5);
+          address.innerHTML = result;
+      });
+
+      let currentPosition = new google.maps.LatLng(latitudeValue, lngitudeValue);
+      let currentMarker = new google.maps.Marker({
+        position: currentPosition,
+        map: map,
+        label: "You are Here!"
+      });
+      
+      map.panTo(currentPosition, currentMarker);
+      map.setZoom(17);
+
     }, function(error) {
       console.error(error);
     }, {
@@ -21,4 +34,17 @@ function getLocation() {
   } else {
     alert('GPS를 지원하지 않습니다');
   }
+}
+
+function changeMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 5,
+    center: currentPosition,
+  });
+
+  new google.maps.Marker({
+    position: currentPosition,
+    map: map,
+    label: "here"
+  });
 }
