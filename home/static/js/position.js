3dc35b424bbcf37e500a5ffd73ad00454f053445
choikwangil <choikwangil0@naver.com>
2020-11-11 16:48:32 +0900

    before push to develop

--- home/static/js/position.js
+++ home/static/js/position.js
@@ -1,29 +1,14 @@
 let position = document.getElementsByClassName("button__position")[0];
 let address = document.getElementsByClassName("location__address")[0];
-let result; // 현재 위치
-let firstlngitudeValue;
-let firstlatitudeValue;
-
-let testPosition1 = document.getElementsByClassName("button__test1")[0];
-let testPosition2 = document.getElementsByClassName("button__test2")[0];
-
-// 테스트 위도, 경도
-let lngitudeTest1 = 127.046553; 
-let latitudeTest1 = 37.280003;
-let lngitudeTest2 = 127.043553;
-let latitudeTest2 = 37.289013;
-
+let result; // 현재 위치 도로명 주소
+let firstlngitudeValue; // 처음 위치 경도
+let firstlatitudeValue; // 처음 위치 위도
 let markers=[]; 
 let latlngs=[];
-let markersLength;
-let latlngsLength;
-let latlng;
-let path;
-let Position, Marker;
+let markersLength, latlngsLength;
+let latlng, path, Position, Marker;
 
-position.addEventListener("click", getLocation);
-testPosition1.addEventListener("click", paintLine1);
-testPosition2.addEventListener("click", paintLine2);
+position.addEventListener("click", firstLocation);
 
 /* @To do
 * 1. 현재 위치만 마커 등록하고 위치 변경 시 해당 마커 삭제 (done)
@@ -32,19 +17,15 @@ testPosition2.addEventListener("click", paintLine2);
 * 4. 현재 위치 marker는 start 버튼 누를 시 출발점이 된다 (gyuri)
 */
 
-// 현재 위치 Get
-function getLocation() {
+// 처음 위치 get
+function firstLocation() {
   if (navigator.geolocation) { // GPS를 지원하면
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition( function(position) {
       firstlngitudeValue = position.coords.longitude;
       firstlatitudeValue = position.coords.latitude;
-      latlng = { lat: firstlatitudeValue, lng: firstlngitudeValue };
-      console.log(latlng);
       markersLength=markers.length;
       latlngsLength=latlngs.length;
-      console.log(markersLength);
-      result = getAddress(firstlatitudeValue, firstlngitudeValue); // 도로명 주소 가져오기 
-      address.innerHTML = result;
+      getAddress(firstlatitudeValue, firstlngitudeValue); // 도로명 주소 가져오기 
       initMarker.setMap(null);  // default marker 삭제
       setMarker(firstlatitudeValue, firstlngitudeValue);  // 현재 위치 마커 생성 및 지도에 등록
       postLatlng(firstlatitudeValue, firstlngitudeValue)
@@ -63,7 +44,6 @@ function getLocation() {
     alert('GPS를 지원하지 않습니다');
   }
 }
-
 // 마커 생성 및 지도에 등록
 function setMarker(lat, lng){
   // @To do 1
@@ -99,11 +79,11 @@ function getAddress(lat, lng){
   axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyDbM2ILxG_n0ScqaBRFcf40fCalno5QX90`)
   .then((res) => {
     result = res.data.results[0].formatted_address.slice(5);
+    address.innerHTML = result;
   });
 
   return result;
 }
-
 // 현재 위치 위도 경도 저장
 function postLatlng(lat, lng){
   // 403 Error를 위한 처리 
@@ -123,7 +103,6 @@ function postLatlng(lat, lng){
     console.log(error);
   })
 }
-
 // 현재 위치와 이동 위치의 선 그어주기
 function paintLine(lat, lng){
   latlng = { lat: lat, lng: lng };
@@ -144,56 +123,4 @@ function paintLine(lat, lng){
   if(latlngsLength==2){
     latlngs.shift();  
   }
-}
-
-// 위치 바뀌었을때 선 그어주기
-function paintLine1(){
-  latlng = { lat: latitudeTest1, lng: lngitudeTest1 };
-  markersLength=markers.length;
-  getAddress(latitudeTest1, lngitudeTest1);
-  setMarker(latitudeTest1, lngitudeTest1, latlng, markersLength);
-
-  // polyline
-  let path = new google.maps.Polyline({
-    path: latlngs,
-    geodesic: true,
-    strokeColor: "#000000",
-    strokeOpacity: 1.0,
-    strokeWeight: 1,
-  });
-  path.setMap(map);
-
-  // 출발 marker 및 line 생성된 latlngs 삭제
-  markersLength=markers.length;
-  if(markersLength==2){
-    markers.shift();
-    latlngs.shift();  
-  }
-}
-
-function paintLine2() {
-  latlng = { lat: latitudeTest2, lng: lngitudeTest2 };
-  markersLength=markers.length;
-  console.log(markersLength);
-
-  getAddress(latitudeTest1, lngitudeTest1);
-  setMarker(latitudeTest2, lngitudeTest2, latlng, 1);
-
-  // polyline
-  let path = new google.maps.Polyline({
-    path: latlngs,
-    geodesic: true,
-    strokeColor: "#000000",
-    strokeOpacity: 1.0,
-    strokeWeight: 1,
-    });
-  path.setMap(map);
-
-  markersLength=markers.length;
-  console.log(markersLength);
-  if(markersLength==2){
-    markers.shift();
-    latlngs.shift();  
-  }
-  console.log(latlngs);
 }
--- home/static/js/tracking.js
+++ home/static/js/tracking.js
@@ -3,22 +3,13 @@ let end = document.getElementsByClassName("button__end")[0];
 start.addEventListener("click", checkBeforeStart);
 end.addEventListener("click", endwatch);
 
-let lngitudeValue;  // 현재 위도
-let latitudeValue;  // 현재 경도
+let lngitudeValue;  // 현재 경도
+let latitudeValue;  // 현재 위도
 var geoId;
 var lastLng = firstlngitudeValue;   // 이전 경도
 var lastLat = firstlatitudeValue;   // 이전 위도
 var lastResult = result;            // 이전 위치
 var intervalobj;
-let t=0;
-
-// Test 1 : 소수점 4번째 자리
-let lat1 = [37.280003, 37.280103, 37.280203, 37.280303, 37.280403, 37.280503, 37.280603, 37.280703];
-let lng1 = [127.046553, 127.046453, 127.046353, 127.046253 ,127.046153, 127.046253, 127.046353, 127.046453];
-
-// Test 2 : 소수점 5번째 자리
-let lat2 = [37.280003, 37.280013, 37.280023, 37.280033, 37.280043, 37.280053, 37.280063, 37.280073];
-let lng2 = [127.046053, 127.046063, 127.046073, 127.046083 ,127.046093, 127.046003, 127.046013, 127.046023];
 
 function checkBeforeStart() {
     var empty = IsTitleEmpty();
@@ -39,45 +30,32 @@ function IsTitleEmpty() {
     else return true;
 }
 
-function test(){
-    latitudeValue = lat2[t];
-    lngitudeValue = lng2[t];
-    watchLocation();
-    lastlng = lngitudeValue;
-    lastLat = latitudeValue
-    t++;
-}
-
 function startWatch() {
-    intervalobj = setInterval(test, 1000);
+    intervalobj = setInterval(watchLocation, 1000);
 }
-
 
 function watchLocation() {
-    // if (navigator.geolocation) { // GPS를 지원하면
-    //     geo_id = navigator.geolocation.getCurrentPosition(function (position) {
-    //         lngitudeValue = position.coords.longitude;
-    //         latitudeValue = position.coords.latitude;
+    navigator.geolocation.getCurrentPosition( function(position) {
+        lngitudeValue = position.coords.longitude;
+        latitudeValue = position.coords.latitude;
+        getAddress(latitudeValue, lngitudeValue);
+    }, function(error) {
+        console.error(error);
+    }, {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: Infinity
+    });
 
-            getAddress(latitudeValue, lngitudeValue);
-
-            if (lastResult != result) {
-                lastResult = result;
-                address.innerHTML = result;
-
-                postLatlng(latitudeValue, lngitudeValue);
-            }
-            latlngsLength=latlngs.length;
-
-            setMarker(latitudeValue, lngitudeValue);
-            paintLine(latitudeValue, lngitudeValue);
-        // });
-    // }
-    // else {
-    //     alert('GPS를 지원하지 않습니다');
-    // }
+    if (lastResult != result) {
+        lastResult = result;
+        setMarker(latitudeValue, lngitudeValue);
+        paintLine(latitudeValue, lngitudeValue);    
+        postLatlng(latitudeValue, lngitudeValue);
+    }
+    markersLength=markers.length;
+    latlngsLength=latlngs.length;
 }
-
 
 function endwatch() {
     clearInterval(intervalobj);
