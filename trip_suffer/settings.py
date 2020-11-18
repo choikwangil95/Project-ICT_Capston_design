2a8bcd17bd95d16ed98fb9ae67d719b8c185b916
choikwangil <choikwangil0@naver.com>
2020-11-18 17:13:27 +0900

    collectstatic

--- trip_suffer/settings.py
+++ trip_suffer/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -126,11 +125,12 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_deploy')
 
 SASS_PROCESSOR_ENABLED = True
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
