9bb4e5c7ae70bebf2fba9176a117dc4d7dc6699f
choikwangil <choikwangil0@naver.com>
2020-11-20 17:49:13 +0900

    Picture model edit

--- home/models.py
+++ home/models.py
@@ -3,26 +3,26 @@ from django.utils import timezone
 
 # Create your models here.
 class Map(models.Model):
-    name = models.CharField(max_length = 30)
-    datetime = models.DateField(default = timezone.now)
-    picture = models.ForeignKey('Picture', on_delete = models.SET_NULL, null = True)
-    zoom = models.FloatField(null = True)
-    centerX = models.FloatField(null = True)
-    centerY = models.FloatField(null = True)
-    
-    def __str__(self):
-        return str(self.pk)
+  name = models.CharField(max_length = 30)
+  datetime = models.DateField(default = timezone.now)
+  zoom = models.FloatField(null = True)
+  centerX = models.FloatField(null = True)
+  centerY = models.FloatField(null = True)
+  
+  def __str__(self):
+      return str(self.pk)
 
 class Gps(models.Model):
-    map_id = models.ForeignKey('Map', on_delete = models.CASCADE, null = True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    datetime = models.DateField(default = timezone.now)
+  map_id = models.ForeignKey('Map', on_delete = models.CASCADE, null = True)
+  latitude = models.FloatField()
+  longitude = models.FloatField()
+  datetime = models.DateField(default = timezone.now)
 
-    # def save(self, *args, **kwargs):
-
+  # def save(self, *args, **kwargs):
 
 class Picture(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    datetime = models.DateField(default = timezone.now)
+  picture = models.ForeignKey('Map', on_delete = models.SET_NULL, null = True)
+  image = models.ImageField(blank=True)
+  latitude = models.FloatField()
+  longitude = models.FloatField()
+  datetime = models.DateField(default = timezone.now)
