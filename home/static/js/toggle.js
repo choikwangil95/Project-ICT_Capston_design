0d12f6b83a286db5b60e19c4d568ec4c23392616
ImYurim <ymyu123@ajou.ac.kr>
2020-11-21 23:49:59 +0900

    main 과 newRoute 생성 분리

---
+++ home/static/js/toggle.js
@@ -0,0 +1,13 @@
+newMap.addEventListener("click", toggleTitle);
+
+function toggleTitle() {
+    if (mapTitle.classList.contains("title--display")) {
+        mapTitle.classList.remove("title--display");
+    } else if (!mapSetTitle.classList.contains("setTitle--display")) {
+        mapTitle.classList.add("title--display");
+    }
+}
+
+if (mapSetTitle.classList.contains("setTitle--display")) {
+    mapSetTitle.classList.remove("setTitle--display");
+}
