97b0a781a8c707500864267525a6a76b40c346b5
ImYurim <ymyu123@ajou.ac.kr>
2020-11-22 00:27:03 +0900

    newRoute 수정

--- home/static/js/toggle.js
+++ home/static/js/toggle.js
@@ -3,6 +3,7 @@ newMap.addEventListener("click", toggleTitle);
 function toggleTitle() {
     if (mapTitle.classList.contains("title--display")) {
         mapTitle.classList.remove("title--display");
+
     } else if (!mapSetTitle.classList.contains("setTitle--display")) {
         mapTitle.classList.add("title--display");
     }
--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('new_route/', views.new_route, name='newroute'),
-    path('create_map/', views.create_map, name='createmap'),
+    path('new_route/create_map/', views.create_map, name='createmap'),
     path('get_mapid/<slug:map_title>/', views.get_map, name='getmap'),
     path('now/<int:map_id>/', views.save_now_geolocation, name='nowloc'),
     path('delete/<int:map_id>/', views.delete_map, name='delmap'),
