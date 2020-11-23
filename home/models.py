19de92289e5640b36342cfcb0c7726f75a88d83a
choikwangil <choikwangil0@naver.com>
2020-11-23 14:49:13 +0900

    conflict edit

--- home/models.py
+++ home/models.py
@@ -15,8 +15,8 @@ class Map(models.Model):
 
 class Gps(models.Model):
     map_id = models.ForeignKey('Map', on_delete=models.CASCADE, null=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
     datetime = models.DateField(default=timezone.now)
 
     # def save(self, *args, **kwargs):
@@ -25,9 +25,9 @@ class Picture(models.Model):
     map_id = models.ForeignKey('Map', on_delete=models.SET_NULL, null=True)
     image = models.ImageField(blank=True, upload_to="picture/original")
     image_thumbnail = ImageSpecField(
-        source='image', processors=[ResizeToFill(200, 130)], format='JPEG')
+        source='image', processors=[ResizeToFill(200, 130)], format='JPEG', options = {'quality':95})
     mapImage_thumbnail = ImageSpecField(
-        source='image', processors=[ResizeToFill(200, 130)], format='JPEG')    
+        source='image', processors=[ResizeToFill(32, 32)], format='JPEG', options = {'quality':95})    
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
     datetime = models.DateField(default=timezone.now)
