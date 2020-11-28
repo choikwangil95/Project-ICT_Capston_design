a6561abfcd90917433609d1279d195371bd36515
ImYurim <ymyu123@ajou.ac.kr>
2020-11-29 02:21:07 +0900

    여행 목록 이미지 띄우기 성공

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.simple_tag
 def get_pic(map_id):
-    pic = Picture.objects.all().filter(map_id=map_id).first().image.file
+    pic = Picture.objects.all().filter(map_id=map_id).first().image.url
     return pic
